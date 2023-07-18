d11e459c219cf3c6ec44e2868e1b6ec8c10501e3
neha.puraswani <neha.puraswani@smartsensesolutions.com>
2023-07-18 10:16:14 +0530

    Code refactor: Leverage parent class email field in ApplicationCustomActionToken

--- src/main/java/eu/gaiax/keycloak/ApplicationCustomActionToken.java
+++ src/main/java/eu/gaiax/keycloak/ApplicationCustomActionToken.java
@@ -10,14 +10,12 @@ public class ApplicationCustomActionToken extends DefaultActionToken {
 
     public static final String TOKEN_TYPE = "execute-actions";
 
-    private static final String JSON_FIELD_REQUIRED_ACTIONS = "rqac";
-    private static final String JSON_FIELD_REDIRECT_URL = "reduri";
+    public static final String JSON_FIELD_REQUIRED_ACTIONS = "rqac";
+
+    public static final String JSON_FIELD_REDIRECT_URL = "reduri";
 
     @JsonProperty(value = JSON_FIELD_REQUIRED_ACTIONS)
     private List<String> rqac;
-
-    @JsonProperty(value = JSON_FIELD_EMAIL)
-    private String email;
 
     @JsonProperty(value = JSON_FIELD_REDIRECT_URL)
     private String redirectUrl;
@@ -25,7 +23,7 @@ public class ApplicationCustomActionToken extends DefaultActionToken {
     public ApplicationCustomActionToken(String userId, String email, String redirectUrl, int absoluteExpirationInSecs, String authenticationSessionId, List<String> rqac) {
         super(userId, TOKEN_TYPE, absoluteExpirationInSecs, null, authenticationSessionId);
         this.rqac = new ArrayList<>(rqac);
-        this.email = email;
+        super.setEmail(email);
         this.redirectUrl = redirectUrl;
     }
 
@@ -38,16 +36,6 @@ public class ApplicationCustomActionToken extends DefaultActionToken {
 
     public void setRqac(List<String> rqac) {
         this.rqac = rqac;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getRedirectUrl() {
